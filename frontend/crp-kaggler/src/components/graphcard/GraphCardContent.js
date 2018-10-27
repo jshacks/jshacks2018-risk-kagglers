1518705a7cd2ed4e1ac0f5891ade1e595621f413
Ciudiii <andreilucian.vaduva@gmail.com>
2018-10-27 19:50:53 +0300

    added chart

--- frontend/crp-kaggler/src/components/graphcard/GraphCardContent.js
+++ frontend/crp-kaggler/src/components/graphcard/GraphCardContent.js
@@ -1,16 +1,74 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 import './graph.css';
+import ReactFC from 'react-fusioncharts';
+
+// Step 3 - Including the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Step 4 - Including the chart type
+import Column2D from 'fusioncharts/fusioncharts.charts';
+
+// Step 5 - Including the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+// Step 6 - Adding the chart as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+
+
+// Step 7 - Creating the JSON object to store the chart configurations
+const chartConfigs = {
+    type: 'column2d',// The chart type
+    width: '700', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: { 
+        // Chart Configuration 
+        "chart": {
+            "caption": "Countries With Most Oil Reserves [2017-18]",
+            "subCaption": "In MMbbl = One Million barrels",
+            "xAxisName": "Country",
+            "yAxisName": "Reserves (MMbbl)",
+            "numberSuffix": "K",
+            "theme": "fusion",
+        },
+        // Chart Data
+        "data": [{
+            "label": "Venezuela",
+            "value": "290"
+        }, {
+            "label": "Saudi",
+            "value": "260"
+        }, {
+            "label": "Canada",
+            "value": "180"
+        }, {
+            "label": "Iran",
+            "value": "140"
+        }, {
+            "label": "Russia",
+            "value": "115"
+        }, {
+            "label": "UAE",
+            "value": "100"
+        }, {
+            "label": "US",
+            "value": "30"
+        }, {
+            "label": "China",
+            "value": "30"
+        }]
+    }
+};
 
 class GraphCardContent extends Component {
-
-    render() {
-        return (
-            <div className="graph-content">
-                graph content
-            </div>
-        );
-    }
+	render() {
+	     return (
+	     <ReactFC
+	        {...chartConfigs}/>
+	     );
+	  }
 
 }
 
