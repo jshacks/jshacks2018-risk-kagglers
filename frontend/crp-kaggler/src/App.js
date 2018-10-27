1ffa8f57acd3a81604ed31a4125b74edd05492ed
vrusanescu <vrusanescu@bitdefender.com>
2018-10-27 14:55:54 +0300

    Add basic react components

--- frontend/crp-kaggler/src/App.js
+++ frontend/crp-kaggler/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        
-        </header>
+        <Header />
+        <div class="main_container">
+          <Menu />
+          <Main />
+        </div>
       </div>
     );
   }
