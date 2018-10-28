7d6fc4dc1aebb809224d69537b7140d617ed2569
Ciudiii <andreilucian.vaduva@gmail.com>
2018-10-28 17:30:36 +0200

    menu button function

--- frontend/crp-kaggler/src/App.js
+++ frontend/crp-kaggler/src/App.js
@@ -5,12 +5,23 @@ import Menu from './components/Menu';
 import Main from './components/Main';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showMenu: true };
+    this.toggleMenu = this.toggleMenu.bind(this);
+
+  }
+
+  toggleMenu = function() {
+    this.setState({ showMenu: !this.state.showMenu });
+  }
+
   render() {
     return (
       <div className="app">
-        <Header />
-        <div className="main_container">
-          <Menu />
+          <Header toggleMenu={this.toggleMenu}/>
+            <div className="main_container">
+          <Menu showMenu={this.state.showMenu}/>
           <Main />
         </div>
       </div>
